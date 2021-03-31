eee977fef4ed7499040c8627dcdd72e244649ff3
TheLucas777 <64582502+TheLucas777@users.noreply.github.com>
2021-03-31 20:42:41 +0200

    openAbhlenen hinzugefüg

--- src/main/java/me/xenodev/tmbbe/event/BewertenEvent.java
+++ src/main/java/me/xenodev/tmbbe/event/BewertenEvent.java
@@ -68,33 +68,7 @@ public class BewertenEvent implements Listener {
             }else if (e.getCurrentItem().getItemMeta().getDisplayName().equals("§cAblehnen")) {
                 p.closeInventory();
                 Inventory inv = Bukkit.createInventory(null, 9*6, "§7>> §6Grund §7<<");
-
-                for(int i = 0; i < 54; i++){
-                    inv.setItem(i, new ItemBuilder(Material.BLACK_STAINED_GLASS_PANE).build());
-                }
-
-                inv.setItem(0, new ItemBuilder(Material.STONE).setName("§6Terra - Forming").setLore("", "§7Versuche dein Terraforming zu Verbessern").build());
-                inv.setItem(9, new ItemBuilder(Material.NETHER_BRICK).setName("§6Terra - Aufbau").setLore("", "§7Verbessere den Aufbau deines Terras").build());
-                inv.setItem(10, new ItemBuilder(Material.OAK_FENCE).setName("§6Terra - Nicht Vorhanden").setLore("", "§7Du musst dein Terra formen").build());
-                inv.setItem(18, new ItemBuilder(Material.WHITE_TERRACOTTA).setName("§6Terra - Coloring").setLore("", "§7Verbessere dein Terracoloring durch passende Blockwahl").build());
-
-                inv.setItem(8, new ItemBuilder(Material.COBBLESTONE_WALL).setName("§6Struktur - Detailsetzung").setLore("", "§7Versuche die Details deiner Struktur besser zu setzen").build());
-                inv.setItem(16, new ItemBuilder(Material.OAK_FENCE).setName("§6Struktur - Details").setLore("", "§7Verbessere die Details deiner Strukturen").build());
-                inv.setItem(17, new ItemBuilder(Material.BRICK_STAIRS).setName("§6Struktur - Dach").setLore("", "§7Der Aufbau deines Dachs ist verbesserungswürdig").build());
-                inv.setItem(26, new ItemBuilder(Material.BRICKS).setName("§6Struktur - Aufbau").setLore("", "§7Verbessere den Aufbau deiner Struktur").build());
-
-                inv.setItem(4, new ItemBuilder(Material.VINE).setName("§6Plot - Vegetation").setLore("", "§7Die Vegetation auf deinem Plot fehlt oder ist schlecht plaziert").build());
-                inv.setItem(12, new ItemBuilder(Material.WHITE_STAINED_GLASS).setName("§6Plot - zu leer").setLore("", "§7Dein Plot fühlt sich zu leer an").build());
-                inv.setItem(13, new ItemBuilder(Material.MOSSY_COBBLESTONE).setName("§6Plot - Zusammenhangslos").setLore("", "§7Der Aufbau deines Plots hat keinen zusammenhang").build());
-                inv.setItem(14, new ItemBuilder(Material.COAL).setName("§6Plot - zu voll").setLore("", "§7Dein Plot fühlt sich zu voll an").build());
-                inv.setItem(22, new ItemBuilder(Material.OAK_LEAVES).setName("§6Mapping - Bäume").setLore("", "§7Die Qualität deiner Bäume entspricht nicht unseren Standards").build());
-
-                inv.setItem(38, new ItemBuilder(Material.LIME_DYE).setName("§aBestätigen").build());
-                OfflinePlayer t = Bukkit.getOfflinePlayer(PlayerEvent.currentplayer.get(p));
-                inv.setItem(40, new ItemBuilder(Material.PLAYER_HEAD).setOwner(PlayerEvent.currentplayer.get(p)).setName("§6" + PlayerEvent.currentplayer.get(p)).setLore("§7Hat sich beworben am", "", "§7Datum: §a" + Bewertungen.getDate(t.getUniqueId().toString()), "§7Zeit: §a" + Bewertungen.getTime(t.getUniqueId().toString())).build());
-                inv.setItem(42, new ItemBuilder(Material.RED_DYE).setName("§cAbbrechen").build());
-
-                p.openInventory(inv);
+                openAblehnen(p,inv);
             } else if (e.getCurrentItem().getItemMeta().getDisplayName().equals("§7Breche den Vorgang ab")) {
                 p.closeInventory();
                 PlayerEvent.currentplayer.remove(p);
@@ -198,6 +172,38 @@ public class BewertenEvent implements Listener {
 
             }
         }
+    }
+
+    private void openAbhlenen(Player p,Inventory inv){
+
+
+        for(int i = 0; i < 54; i++){
+            inv.setItem(i, new ItemBuilder(Material.BLACK_STAINED_GLASS_PANE).build());
+        }
+
+        inv.setItem(0, new ItemBuilder(Material.STONE).setName("§6Terra - Forming").setLore("", "§7Versuche dein Terraforming zu Verbessern").build());
+        inv.setItem(9, new ItemBuilder(Material.NETHER_BRICK).setName("§6Terra - Aufbau").setLore("", "§7Verbessere den Aufbau deines Terras").build());
+        inv.setItem(10, new ItemBuilder(Material.OAK_FENCE).setName("§6Terra - Nicht Vorhanden").setLore("", "§7Du musst dein Terra formen").build());
+        inv.setItem(18, new ItemBuilder(Material.WHITE_TERRACOTTA).setName("§6Terra - Coloring").setLore("", "§7Verbessere dein Terracoloring durch passende Blockwahl").build());
+
+        inv.setItem(8, new ItemBuilder(Material.COBBLESTONE_WALL).setName("§6Struktur - Detailsetzung").setLore("", "§7Versuche die Details deiner Struktur besser zu setzen").build());
+        inv.setItem(16, new ItemBuilder(Material.OAK_FENCE).setName("§6Struktur - Details").setLore("", "§7Verbessere die Details deiner Strukturen").build());
+        inv.setItem(17, new ItemBuilder(Material.BRICK_STAIRS).setName("§6Struktur - Dach").setLore("", "§7Der Aufbau deines Dachs ist verbesserungswürdig").build());
+        inv.setItem(26, new ItemBuilder(Material.BRICKS).setName("§6Struktur - Aufbau").setLore("", "§7Verbessere den Aufbau deiner Struktur").build());
+
+        inv.setItem(4, new ItemBuilder(Material.VINE).setName("§6Plot - Vegetation").setLore("", "§7Die Vegetation auf deinem Plot fehlt oder ist schlecht plaziert").build());
+        inv.setItem(12, new ItemBuilder(Material.WHITE_STAINED_GLASS).setName("§6Plot - zu leer").setLore("", "§7Dein Plot fühlt sich zu leer an").build());
+        inv.setItem(13, new ItemBuilder(Material.MOSSY_COBBLESTONE).setName("§6Plot - Zusammenhangslos").setLore("", "§7Der Aufbau deines Plots hat keinen zusammenhang").build());
+        inv.setItem(14, new ItemBuilder(Material.COAL).setName("§6Plot - zu voll").setLore("", "§7Dein Plot fühlt sich zu voll an").build());
+        inv.setItem(22, new ItemBuilder(Material.OAK_LEAVES).setName("§6Mapping - Bäume").setLore("", "§7Die Qualität deiner Bäume entspricht nicht unseren Standards").build());
+
+        inv.setItem(38, new ItemBuilder(Material.LIME_DYE).setName("§aBestätigen").build());
+        OfflinePlayer t = Bukkit.getOfflinePlayer(PlayerEvent.currentplayer.get(p));
+        inv.setItem(40, new ItemBuilder(Material.PLAYER_HEAD).setOwner(PlayerEvent.currentplayer.get(p)).setName("§6" + PlayerEvent.currentplayer.get(p)).setLore("§7Hat sich beworben am", "", "§7Datum: §a" + Bewertungen.getDate(t.getUniqueId().toString()), "§7Zeit: §a" + Bewertungen.getTime(t.getUniqueId().toString())).build());
+        inv.setItem(42, new ItemBuilder(Material.RED_DYE).setName("§cAbbrechen").build());
+
+
+        p.openInventory(inv);
     }
 
 }
